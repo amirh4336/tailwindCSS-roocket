1c30276522cc37f241b79a2330dce6683f07de76
amir <amirhossienheydarpoor@gmail.com>
2022-12-05 21:34:18 +0330

    add screen tablet

--- project-1/tailwind.config.js
+++ project-1/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'tablet': '640px',
+      }
+    },
   },
   plugins: [],
 }
